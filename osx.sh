c4163e28f8ea813a62b24948ae416c83e84e0e0e
Thomas Nichols <thomas.nichols89@gmail.com>
2018-01-27 13:16:58 -0600

    Updates

--- osx.sh
+++ osx.sh
@@ -7,7 +7,7 @@ osascript -e 'tell application "System Preferences" to quit'
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
@@ -150,11 +150,8 @@ defaults write com.apple.dashboard mcx-disabled -bool true
 # Don’t show Dashboard as a Space
 defaults write com.apple.dock dashboard-in-overlay -bool true
 
-# Don’t automatically rearrange Spaces based on most recent use
-defaults write com.apple.dock mru-spaces -bool false
-
 ###############################################################################
-# Terminal & iTerm 2                                                          #
+# Terminal                                                         #
 ###############################################################################
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
--- runcom/.bash_profile
+++ runcom/.bash_profile
@@ -29,3 +29,6 @@ fi;
 
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
 [ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2- | tr ' ' '\n')" scp sftp ssh;
+
+# added by Anaconda3 5.0.1 installer
+export PATH="/Users/tanichols/anaconda3/bin:$PATH"
